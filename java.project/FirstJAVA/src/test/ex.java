36ca02fb3d9333d2eaf0086f08a404b37c3b15ad
kimnh95 <57422135+kimnh95@users.noreply.github.com>
2019-11-06 10:41:13 +0900

    java_project

---
+++ java.project/FirstJAVA/src/test/ex.java
@@ -0,0 +1,19 @@
+package test;
+
+public class ex {
+
+	
+	public static void main(String[] args)        {
+		int num1 = (int) Math.random();
+		int num2 = (int) Math.random();
+		int num;
+		
+		
+	 }
+	 public static void num(int num1, int num2) {
+		 System.out.println("덧셈 : (int) (num1 + num2)");
+		 
+			 }        
+		   
+}
+
